65898d06cb9294b5161e5b8750ba3cc5f56a691b
xp-vit <xp.vit.lr@gmail.com>
2013-10-26 18:04:56 +0300

    Add first simple Android unit test which runs

---
+++ src/instrumentTest/java/com/eucsoft/foodex/test/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.eucsoft.foodex.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+import com.eucsoft.foodex.MainActivity;
+import com.eucsoft.foodex.R;
+
+import static android.test.ViewAsserts.assertOnScreen;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    //^Activity to test
+    private MainActivity foodexMainActivity;
+    private Button takePictureButton;
+
+    // Be careful about letting the IDE create the constructor.  As of this writing,
+    // it creates a constructor that's compiles cleanly but doesn't run any tests
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        foodexMainActivity = getActivity();
+        takePictureButton = (Button) foodexMainActivity.findViewById(R.id.takePictureButton);
+    }
+
+    // Methods whose names are prefixed with test will automatically be run
+    public void testTakePictureButtonExist() {
+        assertOnScreen(foodexMainActivity.getWindow().getDecorView(), takePictureButton);
+    }
+
+}
