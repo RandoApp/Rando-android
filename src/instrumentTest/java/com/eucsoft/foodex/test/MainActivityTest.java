1cb135cdc37447453ccf5354eae80883c07566ad
xp-vit <xp.vit.lr@gmail.com>
2013-10-27 22:40:54 +0300

    Remove failing test

--- src/instrumentTest/java/com/eucsoft/foodex/test/MainActivityTest.java
+++ src/instrumentTest/java/com/eucsoft/foodex/test/MainActivityTest.java
@@ -31,9 +31,4 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         assertOnScreen(foodexMainActivity.getWindow().getDecorView(), takePictureButton);
     }
 
-    // Methods whose names are prefixed with test will automatically be run
-    public void testTakePictureButtonExistFail() {
-        assertOnScreen(foodexMainActivity.getWindow().getDecorView(), null);
-    }
-
 }
