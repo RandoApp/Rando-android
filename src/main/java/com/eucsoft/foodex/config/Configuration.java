15d1948067bc4ae05ce9c19bc9b6034a5a666f01
xp-vit <xp.vit.lr@gmail.com>
2013-11-01 21:10:31 +0300

    Add UploadService stub Rework Crop and Save Travis Update to Android KitKat

--- src/main/java/com/eucsoft/foodex/config/Configuration.java
+++ src/main/java/com/eucsoft/foodex/config/Configuration.java
@@ -3,5 +3,7 @@ package com.eucsoft.foodex.config;
 public class Configuration {
 
     public static final String ALBUM_NAME = "Foodex";
-    public static final String ALBUM_FOLDER = "Foodex/media/" + ALBUM_NAME;
+    public static final String UPLOAD_SERVICE_NOTIFICATION = "UPLOAD.SERVICE.NOTIFICATION";
+    public static final String RESULT = "RESULT";
+    public static final String FILENAME = "FILENAME";
 }
