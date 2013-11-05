52b6d53491f743662b4dd815f1b6f77598aac0f9
xp-vit <xp.vit.lr@gmail.com>
2013-11-05 16:15:16 +0300

    Add Task stabs refactoring

--- src/main/java/com/eucsoft/foodex/MainActivity.java
+++ src/main/java/com/eucsoft/foodex/MainActivity.java
@@ -1,19 +1,12 @@
 package com.eucsoft.foodex;
 
-import android.net.Uri;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import com.eucsoft.foodex.fragment.AuthFragment;
-import com.eucsoft.foodex.view.FoodView;
 
 public class MainActivity extends ActionBarActivity {
 
