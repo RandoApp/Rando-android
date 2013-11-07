6c8fefbf4e6e59cf7aa258da79a1640abf4f9914
dimhold <dimhold@gmail.com>
2013-11-07 20:36:03 +0300

    Modify API.bonAppetit transfer params method

--- src/main/java/com/eucsoft/foodex/Constants.java
+++ src/main/java/com/eucsoft/foodex/Constants.java
@@ -50,5 +50,5 @@ public class Constants {
     public static final String REPORT_URL = SERVER_URL + "/report";
 
 
-    public static final String BON_APPETIT_URL = SERVER_URL + "/bonappetit";
+    public static final String BON_APPETIT_URL = SERVER_URL + "/bonappetit/";
 }
