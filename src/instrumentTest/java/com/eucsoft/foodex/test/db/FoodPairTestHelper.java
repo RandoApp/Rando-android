dfa7af361ca7e22ec5e62ac814f1b3485a22f19a
xp-vit <xp.vit.blr@gmail.com>
2013-12-03 22:48:41 +0300

    add fogotten File

---
+++ src/instrumentTest/java/com/eucsoft/foodex/test/db/FoodPairTestHelper.java
@@ -0,0 +1,74 @@
+package com.eucsoft.foodex.test.db;
+
+import com.eucsoft.foodex.db.model.FoodPair;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+public class FoodPairTestHelper {
+    private static Random random = new Random();
+
+    public static List<FoodPair> getNRandomFoodPairs(int n) {
+        Date baseDate = new Date();
+
+        List<FoodPair> foodPairs = new ArrayList<FoodPair>();
+        for (int i = 0; i < n; i++) {
+            FoodPair foodPair;
+            Date userDate = new Date();
+            userDate.setTime(baseDate.getTime() + random.nextInt(1000000));
+
+            foodPair = new FoodPair();
+            foodPair.user.foodURL = "blaURL" + i;
+            foodPair.user.mapURL = "blaFile" + i;
+            foodPair.user.bonAppetit = 0;
+            foodPair.user.foodDate = userDate;
+
+            Date strangerDate = new Date();
+            strangerDate.setTime(baseDate.getTime() + random.nextInt(1000000));
+            foodPair.stranger.foodURL = "Bla2URL" + i;
+            foodPair.stranger.mapURL = "LocalFileStranger" + i;
+            foodPair.stranger.bonAppetit = 0;
+            foodPair.stranger.foodDate = strangerDate;
+            foodPairs.add(foodPair);
+        }
+
+        return foodPairs;
+    }
+
+    public static FoodPair getRandomFoodPair() {
+
+        FoodPair foodPair;
+        Date userDate = new Date();
+        userDate.setTime(new Date().getTime() + random.nextInt(1000000));
+
+        foodPair = new FoodPair();
+        foodPair.user.foodURL = "blaURL";
+        foodPair.user.mapURL = "blaFile";
+        foodPair.user.bonAppetit = 0;
+        foodPair.user.foodDate = userDate;
+
+        Date strangerDate = new Date();
+        strangerDate.setTime(new Date().getTime() + random.nextInt(1000000));
+        foodPair.stranger.foodURL = "Bla2URL";
+        foodPair.stranger.mapURL = "LocalFileStranger";
+        foodPair.stranger.bonAppetit = 0;
+        foodPair.stranger.foodDate = strangerDate;
+        return foodPair;
+    }
+
+    public static void checkListNaturalOrder(List<FoodPair> foodPairs) {
+        FoodPair prevPair = null;
+        for (FoodPair foodPair : foodPairs) {
+            if (prevPair != null) {
+                assertThat("Order is broken: " + foodPair.user.foodDate.toString() + " is greater Than " + prevPair.user.foodDate.toString(), foodPair.user.foodDate, greaterThanOrEqualTo(prevPair.user.foodDate));
+            } else {
+                prevPair = foodPair;
+            }
+        }
+    }
+}
