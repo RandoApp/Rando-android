bc78c0b232cfd0816a700c0a3b25fdbc3d124c53
dimhold <dimhold@gmail.com>
2013-12-04 18:48:08 +0300

    Fix unit tests in PreferencesTest

--- src/instrumentTest/java/com/eucsoft/foodex/test/PreferencesTest.java
+++ src/instrumentTest/java/com/eucsoft/foodex/test/PreferencesTest.java
@@ -1,9 +1,13 @@
 package com.eucsoft.foodex.test;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.test.AndroidTestCase;
 
+import static com.eucsoft.foodex.Constants.*;
 import com.eucsoft.foodex.MainActivity;
 import com.eucsoft.foodex.preferences.Preferences;
+import static com.eucsoft.foodex.preferences.Preferences.*;
 
 import java.util.UUID;
 
@@ -15,32 +19,64 @@ public class PreferencesTest extends AndroidTestCase {
     @Override
     protected void setUp() throws Exception {
         MainActivity.context = this.getContext();
+        MainActivity.context.getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE).edit().clear().commit();
     }
 
-    public void testSetGetSessionCookie() {
-        String newSessionCookie = UUID.randomUUID().toString();
-        Preferences.setSessionCookie(cookie.getValue(), cookie.getDomain(), newSessionCookie, cookie.getExpiryDate());
-        assertThat(Preferences.getSessionCookie(), is(newSessionCookie));
+    public void testGetSessionCookie() {
+        String value = UUID.randomUUID().toString();
+        String domain = "domain.com";
+        String path = "/";
+
+        Preferences.setSessionCookie(value, domain, path);
+        assertThat(Preferences.getSessionCookieValue(), is(value));
+        assertThat(Preferences.getSessionCookieDomain(), is(domain));
+        assertThat(Preferences.getSessionCookiePath(), is(path));
     }
 
-    public void testSetGetSessionCookieEmpty() {
-        String newSessionCookie = "";
-        Preferences.setSessionCookie(cookie.getValue(), cookie.getDomain(), newSessionCookie, cookie.getExpiryDate());
-        assertThat(Preferences.getSessionCookie(), is(newSessionCookie));
+    public void testGetSessionCookieSetEmptyValueDomainAndPath() {
+        String value = "";
+        String domain = "";
+        String path = "";
+
+        Preferences.setSessionCookie(value, domain, path);
+        assertThat(Preferences.getSessionCookieValue(), is(value));
+        assertThat(Preferences.getSessionCookieDomain(), is(domain));
+        assertThat(Preferences.getSessionCookiePath(), is(path));
     }
 
-    public void testSetGetSessionCookieNull() {
-        String newSessionCookie = null;
-        Preferences.setSessionCookie(cookie.getValue(), cookie.getDomain(), newSessionCookie, cookie.getExpiryDate());
-        assertThat(Preferences.getSessionCookie(), is(Preferences.SEESSION_COOKIE_DEFAULT_VALUE));
+    public void testGetSessionCookieSetCookieValueAsNull() {
+        String domain = "domain.com";
+        String path = "/";
+
+        Preferences.setSessionCookie(null, domain, path);
+        assertThat(Preferences.getSessionCookieValue(), is(SESSION_COOKIE_DEFAULT_VALUE));
+        assertThat(Preferences.getSessionCookieDomain(), is(SERVER_HOST));
+        assertThat(Preferences.getSessionCookiePath(), is(SESSION_COOKIE_PATH_DEFAULT_VALUE));
+    }
+
+    public void testGetSessionCookieSetDomainAndPathAsNull() {
+        String value = UUID.randomUUID().toString();
+        Preferences.setSessionCookie(value, null, null);
+        assertThat(Preferences.getSessionCookieValue(), is(value));
+        assertThat(Preferences.getSessionCookieDomain(), is(SERVER_HOST));
+        assertThat(Preferences.getSessionCookiePath(), is(SESSION_COOKIE_PATH_DEFAULT_VALUE));
     }
 
     public void testRemoveSessionCookie() {
-        String newSessionCookie = UUID.randomUUID().toString();
-        Preferences.setSessionCookie(cookie.getValue(), cookie.getDomain(), newSessionCookie, cookie.getExpiryDate());
-        assertThat(Preferences.getSessionCookie(), is(newSessionCookie));
+        String value = UUID.randomUUID().toString();
+        String domain = "domain.com";
+        String path = "/";
+
+        Preferences.setSessionCookie(value, domain, path);
+        assertThat(Preferences.getSessionCookieValue(), is(value));
+        assertThat(Preferences.getSessionCookieDomain(), is(domain));
+        assertThat(Preferences.getSessionCookiePath(), is(path));
+
         Preferences.removeSessionCookie();
-        assertThat(Preferences.getSessionCookie(), is(Preferences.SEESSION_COOKIE_DEFAULT_VALUE));
+
+        assertThat(Preferences.getSessionCookieValue(), is(SESSION_COOKIE_DEFAULT_VALUE));
+        assertThat(Preferences.getSessionCookieDomain(), is(SERVER_HOST));
+        assertThat(Preferences.getSessionCookiePath(), is(SESSION_COOKIE_PATH_DEFAULT_VALUE));
     }
 
     //This test should pass untill we implement Training logic
