1212c0694caf5402bba128bae202a5eb556bfec5
dimhold <dimhold@gmail.com>
2013-12-25 16:56:13 +0300

    Add report menu item

---
+++ src/main/java/com/eucsoft/foodex/menu/ReportMenu.java
@@ -0,0 +1,12 @@
+package com.eucsoft.foodex.menu;
+
+import com.eucsoft.foodex.R;
+
+public class ReportMenu {
+
+    public static final int ID =  R.id.action_report;
+
+    public void select () {
+
+    }
+}
