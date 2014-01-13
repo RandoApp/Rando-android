dfa840ae5059204c16f3e877557efa1ee5ac583e
xp-vit <xp.vit.blr@gmail.com>
2014-01-14 01:13:02 +0300

    Merge branch 'impl-cache'
    Conflicts:
    	src/main/java/com/eucsoft/foodex/menu/Menu.java
    	src/main/java/com/eucsoft/foodex/menu/ReportMenu.java

--- src/main/java/com/eucsoft/foodex/menu/Menu.java
+++ src/main/java/com/eucsoft/foodex/menu/Menu.java
@@ -1,13 +1,16 @@
 package com.eucsoft.foodex.menu;
 
 import android.app.Activity;
+import android.view.MenuItem;
 
 public abstract class Menu {
 
     protected Activity activity;
+    protected MenuItem menuItem;
 
-    public Menu(Activity activity) {
+    public Menu(MenuItem menuItem, Activity activity) {
         this.activity = activity;
+        this.menuItem = menuItem;
     }
 
     public abstract void select();
