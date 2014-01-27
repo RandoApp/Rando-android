63043651aafb839c5eb899116157271612ccf4ea
dimhold <dimhold@gmail.com>
2014-01-27 18:01:12 +0300

    Refactore GoogleAuthTask

--- src/main/java/com/eucsoft/foodex/task/GoogleAuthTask.java
+++ src/main/java/com/eucsoft/foodex/task/GoogleAuthTask.java
@@ -1,7 +1,7 @@
 package com.eucsoft.foodex.task;
 
 import com.eucsoft.foodex.App;
-import com.eucsoft.foodex.task.BaseTask;
+import com.eucsoft.foodex.log.Log;
 import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.auth.GooglePlayServicesAvailabilityException;
@@ -23,25 +23,33 @@ public class GoogleAuthTask extends BaseTask {
     public Integer run() {
         try {
             String token = fetchToken(email);
-            data.put("token", token);
-            return OK;
+            if (token != null) {
+                data.put("email", email);
+                data.put("token", token);
+                return OK;
+            } else {
+                data.put("error", "Problem with Google service. Please try again.");
+                return ERROR;
+            }
+        } catch (GooglePlayServicesAvailabilityException playEx) {
+            data.put("error", "Problem with Google service. Please try again.");
+            Log.e(GoogleAuthTask.class, "Google Play service exception: ", playEx.getMessage());
+        } catch (UserRecoverableAuthException userRecoverableException) {
+            data.put("error", "Problem with Google service. Please, fix problem and try again.");
+            Log.e(GoogleAuthTask.class, "Unable to authenticate, but the user can fix this: ", userRecoverableException.getMessage());
+        } catch (GoogleAuthException fatalException) {
+            data.put("error", "Problem with Google service. Please try again.");
+            Log.e(GoogleAuthTask.class, "Unrecoverable error " + fatalException.getMessage());
         } catch (IOException ex) {
-            data.put("error", "Following Error occured, please try again. " + ex.getMessage());
-            return ERROR;
+            data.put("error", "Problem with Google service. Please try again.");
+            Log.e(GoogleAuthTask.class, "IOException when fetch google token: " + ex.getMessage());
         }
+
+        return ERROR;
     }
 
-    private String fetchToken(String email) throws IOException {
-      try {
+    private String fetchToken(String email) throws IOException, GoogleAuthException {
         return GoogleAuthUtil.getToken(App.context, email, SCOPE);
-      } catch (GooglePlayServicesAvailabilityException playEx) {
-      } catch (UserRecoverableAuthException userRecoverableException) {
-        // Unable to authenticate, but the user can fix this.
-        // Forward the user to the appropriate activity.
-      } catch (GoogleAuthException fatalException) {
-//        onError("Unrecoverable error " + fatalException.getMessage(), fatalException);
-      }
-      return null;
     }
 
 }
