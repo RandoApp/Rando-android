aeaf952a414872ee09365742c5a0848ac13af71e
xp-vit <xp.vit.blr@gmail.com>
2014-06-02 18:43:33 +0300

    Add some params to CameraHost

--- src/main/java/com/github/randoapp/camera/CameraCaptureFragment.java
+++ src/main/java/com/github/randoapp/camera/CameraCaptureFragment.java
@@ -6,18 +6,23 @@ import android.content.Intent;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.commonsware.cwac.camera.CameraHost;
+import com.commonsware.cwac.camera.CameraUtils;
 import com.commonsware.cwac.camera.CameraView;
 import com.commonsware.cwac.camera.PictureTransaction;
 import com.commonsware.cwac.camera.SimpleCameraHost;
 import com.commonsware.cwac.camera.acl.CameraFragment;
 import com.github.randoapp.R;
+import com.github.randoapp.log.Log;
 import com.github.randoapp.util.CameraUtil;
 import com.github.randoapp.util.FileUtil;
 
@@ -47,10 +52,18 @@ public class CameraCaptureFragment extends CameraFragment {
 
         cameraView = (CameraView) rootView.findViewById(R.id.camera);
         cameraView.setHost(new RandoCameraHost(getActivity().getBaseContext()));
+
+        WindowManager windowManager = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        int displayWidth = display.getWidth();
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(displayWidth, display.getHeight());
+        cameraView.setLayoutParams(layoutParams);
         setCameraView(cameraView);
 
         captureButton = (ImageView) rootView.findViewById(R.id.capture_button);
         captureButton.setOnClickListener(new CaptureButtonListener());
+
 
         return rootView;
     }
@@ -74,7 +87,17 @@ public class CameraCaptureFragment extends CameraFragment {
 
         @Override
         public Camera.Size getPictureSize(PictureTransaction xact, Camera.Parameters parameters) {
-            return CameraUtil.getBestPictureSizeForOldDevices(parameters.getSupportedPictureSizes());
+            Camera.Size size = CameraUtil.getBestPictureSizeForOldDevices(parameters.getSupportedPictureSizes());
+            Log.i(CameraCaptureFragment.class, "Previev size:", String.valueOf(size.height), "x", String.valueOf(size.width));
+            return size;
+        }
+
+        @Override
+        public Camera.Size getPreviewSize(int displayOrientation, int width, int height, Camera.Parameters parameters) {
+            Camera.Size size = CameraUtils.getBestAspectPreviewSize(displayOrientation, width, height, parameters);
+            Log.i(CameraCaptureFragment.class, "Previev screen size:", String.valueOf(height), "x", String.valueOf(width));
+            Log.i(CameraCaptureFragment.class, "Previev size:", String.valueOf(size.height), "x", String.valueOf(size.width));
+            return size;
         }
 
         @Override
@@ -127,5 +150,9 @@ public class CameraCaptureFragment extends CameraFragment {
             return false;
         }
 
+        @Override
+        public boolean useFullBleedPreview() {
+            return false;
+        }
     }
 }
