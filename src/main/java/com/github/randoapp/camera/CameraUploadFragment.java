fc0508a776d04b7a5302f3049df922e5ae866ec9
dimhold <dimhold@gmail.com>
2014-06-25 16:12:32 +0300

    Delete CropImageTask

--- src/main/java/com/github/randoapp/camera/CameraUploadFragment.java
+++ src/main/java/com/github/randoapp/camera/CameraUploadFragment.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -43,33 +45,17 @@ public class CameraUploadFragment extends SherlockFragment {
 
         preview = (RoundedImageView) rootView.findViewById(R.id.preview);
 
-        Bundle bundle = getArguments();
-        String fileToCrop = bundle.getString(Constants.FILEPATH);
+        WindowManager windowManager = (WindowManager) getActivity().getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        int previewSize = Math.min(display.getWidth(), display.getHeight());
 
-        prepareForUpload(fileToCrop);
+        Bundle bundle = getArguments();
+        picFileName = bundle.getString(Constants.FILEPATH);
+        preview.setImageBitmap(BitmapUtil.decodeSampledBitmap(picFileName, previewSize, previewSize));
 
         uploadButton = (ImageView) rootView.findViewById(R.id.upload_button);
         uploadButton.setOnClickListener(new UploadButtonListner());
         return rootView;
-    }
-
-
-    private void prepareForUpload(String fileToCrop) {
-        new CropImageTask(fileToCrop)
-                .onOk(new OnOk() {
-                    @Override
-                    public void onOk(Map<String, Object> data) {
-                        picFileName = (String) data.get(Constants.FILEPATH);
-                        preview.setImageBitmap(BitmapUtil.decodeSampledBitmap(picFileName, preview.getWidth(), preview.getWidth()));
-                    }
-                })
-                .onError(new OnError() {
-                    @Override
-                    public void onError(Map<String, Object> data) {
-                        Log.e(CropImageTask.class, "Can not crop image");
-                    }
-                })
-                .execute();
     }
 
     private class UploadButtonListner implements View.OnClickListener {
