d0db09fee280402518bebb8410873bdca9ed0fce
Viktar Patotski <xp.vit.blr@gmail.com>
2016-07-06 11:29:18 +0300

    update waiter script

--- ci/wait_for_emulator.sh
+++ ci/wait_for_emulator.sh
@@ -12,8 +12,8 @@ until [[ "$bootanim" =~ "stopped" ]]; do
   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
     || "$bootanim" =~ "running" ]]; then
-    let "failcounter += 1"
-    echo "Waiting for emulator to start: Waiting for $failcounter seconds while timeout is $timeout_in_sec seconds"
+    let "failcounter += 5"
+    echo "Waiting for $failcounter seconds while timeout is $timeout_in_sec seconds"
     if [[ $failcounter -gt timeout_in_sec ]]; then
       echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
       exit 1
