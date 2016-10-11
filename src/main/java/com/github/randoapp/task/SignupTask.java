189fe17107d55ec42b2ce8b4e9a34f6c830ba9e4
Viktar Patotski <xp.vit.blr@gmail.com>
2016-10-11 16:04:06 +0300

    Fix account not set on Setting Screen

--- src/main/java/com/github/randoapp/task/SignupTask.java
+++ src/main/java/com/github/randoapp/task/SignupTask.java
@@ -2,6 +2,7 @@ package com.github.randoapp.task;
 
 import com.github.randoapp.Constants;
 import com.github.randoapp.api.API;
+import com.github.randoapp.preferences.Preferences;
 
 public class SignupTask extends BaseTask {
 
@@ -16,6 +17,7 @@ public class SignupTask extends BaseTask {
     public Integer run() {
         try {
             API.signup(email, password);
+            Preferences.setAccount(email);
             return OK;
         } catch (Exception exc) {
             data.put(Constants.ERROR, exc.getMessage());
