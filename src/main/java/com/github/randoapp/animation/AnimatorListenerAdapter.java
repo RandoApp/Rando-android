8456ef7c37a630eb22d15ce7d8fe93664fd0f9ce
Viktar Patotski <xp.vit.blr@gmail.com>
2017-05-06 17:19:28 +0300

    fix codacy

--- src/main/java/com/github/randoapp/animation/AnimatorListenerAdapter.java
+++ src/main/java/com/github/randoapp/animation/AnimatorListenerAdapter.java
@@ -6,21 +6,21 @@ public class AnimatorListenerAdapter implements  Animator.AnimatorListener{
 
     @Override
     public void onAnimationStart(Animator animation) {
-
+        //implement if necessary
     }
 
     @Override
     public void onAnimationEnd(Animator animation) {
-
+        //implement if necessary
     }
 
     @Override
     public void onAnimationCancel(Animator animation) {
-
+        //implement if necessary
     }
 
     @Override
     public void onAnimationRepeat(Animator animation) {
-
+        //implement if necessary
     }
 }
