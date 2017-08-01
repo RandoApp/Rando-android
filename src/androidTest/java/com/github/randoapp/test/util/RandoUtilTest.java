a0ae1d340f17c2e74390b4d7e66d49caf2ce2c8a
Viktar Patotski <xp.vit.blr@gmail.com>
2017-08-01 12:32:26 +0300

    fix build

--- src/androidTest/java/com/github/randoapp/test/util/RandoUtilTest.java
+++ src/androidTest/java/com/github/randoapp/test/util/RandoUtilTest.java
@@ -6,20 +6,11 @@ import android.support.test.filters.SmallTest;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.github.randoapp.db.RandoDAO;
-import com.github.randoapp.db.model.Rando;
-import com.github.randoapp.test.db.RandoTestHelper;
-import com.github.randoapp.util.RandoUtil;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
@@ -38,5 +29,10 @@ public class RandoUtilTest {
     public void tearDown() throws Exception {
         RandoDAO.clearRandos(context);
         RandoDAO.clearRandoToUpload(context);
+    }
+
+    @Test
+    public void dummy(){
+        
     }
 }
